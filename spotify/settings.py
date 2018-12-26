dc0479ef53267246f62506f5ee4887d21dacc400
Matheus Hernandes <midia.matheus@gmail.com>
2018-12-25 23:02:44 -0200

    change config file

--- spotify/settings.py
+++ spotify/settings.py
@@ -1,18 +1,48 @@
 import yaml
 import os
 
-current_dir = os.path.dirname(os.path.realpath(__file__))
-config_filepath = os.path.join(current_dir, "settings.yaml")
+config_dir = os.path.join(os.path.expanduser("~"), ".config/spotify-cli/")
+config_filepath = os.path.join(config_dir, "settings.yaml")
 
-with open(config_filepath, "r") as conf:
-    y = yaml.load(conf)
+
+def ensure_config_file():
+    default_settings = {
+        "SPOTIPY_CLIENT_ID": "",
+        "SPOTIPY_CLIENT_SECRET": "",
+        "SPOTIPY_REDIRECT_URI": "",
+        "SPOTIPY_SCOPES": [
+            "user-read-playback-state",
+            "user-read-currently-playing",
+            "user-modify-playback-state",
+            "user-read-private",
+            "streaming",
+        ],
+        "SPOTIPY_TOKEN": "",
+        "SPOTIPY_USERNAME": "",
+    }
+    if not os.path.isdir(config_dir):
+        os.mkdir(config_dir)
+
+    if not os.path.exists(config_filepath):
+        with open(config_filepath, "w") as outfile:
+            return yaml.dump(default_settings, outfile, default_flow_style=False)
+
+
+ensure_config_file()
+
+
+def get_config():
+    with open(config_filepath, "r") as conf:
+        return yaml.load(conf)
 
 
 def get(k):
+    y = get_config()
     return y.get(k)
 
 
 def set(data):
+    y = get_config()
     for k, v in y.items():
         if data.get(k, None) is None:
             data[k] = v
@@ -21,10 +51,12 @@ def set(data):
 
 
 def get_all():
+    y = get_config()
     return y
 
 
 def ensure_all():
+    y = get_config()
     """Ensure all values are set
     :returns: True/False
 
@@ -33,7 +65,7 @@ def ensure_all():
         "SPOTIPY_CLIENT_ID",
         "SPOTIPY_CLIENT_SECRET",
         "SPOTIPY_REDIRECT_URI",
-        "SPOTIPY_USERNAME"
+        "SPOTIPY_USERNAME",
     ]
     values = [y.get(k, False) for k in keys]
     return all(values)
